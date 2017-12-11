9cd1f02a9eddbb7003307ac72dbd41523a322fe7
Mesa Marin Robin <robin.mesa-marin@eemi.com>
2017-12-11 17:07:30 +0100

    Added style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
+//Clock function
 function startTime() {
-
     //Variables
     var today = new Date();
     var hours = today.getHours();
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
-    var color = "#" +  hours + minutes + seconds;
+    var color = "#" +  hours + minutes + seconds; //Color contain a string with the hexadecimal code of the color
 
     minutes = checkTime(minutes);
     seconds = checkTime(seconds);
 
-    document.getElementById('hours').innerHTML = hours;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerHTML = seconds;
+    document.getElementById('hours').innerHTML =  '<span id="hoursValue" class="' + hours + '">' + hours + '</span>';
+    document.getElementById('minutes').innerHTML =  '<span id="minutesValue" class="' + minutes + '">' + minutes + '</span>';
+    document.getElementById('seconds').innerHTML = '<span id="secondsValue" class="' + seconds + '">' + seconds + '</span>';
 
     var loop = setTimeout(startTime, 1000);
 
     //Console log section
     console.log("Color : " + color);
-    console.log("Hours : " + hours);
-    console.log("Minutes : " + minutes);
-    console.log("Seconds : " + seconds);
+    //console.log("Hours : " + hours);
+    //console.log("Minutes : " + minutes);
+    //console.log("Seconds : " + seconds);
 
     document.getElementById('test').style.background = color;
 }
@@ -29,5 +29,6 @@ function checkTime(number) {
         number = "0" + number}  // add zero in front of numbers < 10
     return number;
 }
+//TODO: Add a copy to clipboard function
 
 
