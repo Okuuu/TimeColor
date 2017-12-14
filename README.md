d6c15ab3ce57de8fdfc7e3dd8f5066baf3908212
Mesa Marin Robin <robin.mesa-marin@eemi.com>
2017-12-14 21:34:23 +0100

    Improve the JS + add the copy function

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TimeColor
-⏲️ A clock that display the hour in hexadecimal color
+⏲️ A clock that display the time in hexadecimal color (in the format "#hhmmss")
+
+## Technical Information
+- I used javascript vanilla (no JQuery)
+- I used [alertify](http://alertifyjs.com/) for handling the alerts
+- The font is [Quicksand Sans Serif](https://fonts.google.com/specimen/Quicksand) from GoogleFont
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 //Clock function
+const storage = document.getElementById('storage');
+
 function startTime() {
+
+    var hoursDiv = document.getElementById('hours');
+    var minutesDiv = document.getElementById('minutes');
+    var secondsDiv = document.getElementById('seconds');
+    var obj = getColor();
+
+    //Console log section
+    console.log("Color : " + obj.color);
+    console.log("Hours : " + obj.hours);
+    console.log("Minutes : " + obj.minutes);
+    console.log("Seconds : " + obj.seconds);
+
+    var hours = ""+obj.hours;
+
     //Variables
-    var today = new Date();
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
-    var color = "#" +  hours + minutes + seconds; //Color contain a string with the hexadecimal code of the color
-
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-
-    document.getElementById('hours').innerHTML =  '<span id="hoursValue" class="' + hours + '">' + hours + '</span>';
-    document.getElementById('minutes').innerHTML =  '<span id="minutesValue" class="' + minutes + '">' + minutes + '</span>';
-    document.getElementById('seconds').innerHTML = '<span id="secondsValue" class="' + seconds + '">' + seconds + '</span>';
+    hoursDiv.innerHTML =  '<span id="hoursValue">' + obj.hours + '</span>';
+    minutesDiv.innerHTML =  '<span id="minutesValue">' + obj.minutes + '</span>';
+    secondsDiv.innerHTML = '<span id="secondsValue">' + obj.seconds + '</span>';
 
     var loop = setTimeout(startTime, 1000);
 
-    //Console log section
-    console.log("Color : " + color);
-    //console.log("Hours : " + hours);
-    //console.log("Minutes : " + minutes);
-    //console.log("Seconds : " + seconds);
-
-    document.getElementById('test').style.background = color;
+    document.getElementById('test').style.background = obj.color;
 }
 function checkTime(number) {
     if (number < 10){
@@ -32,8 +33,52 @@ function checkTime(number) {
 
 //TODO: Add the information button
 function info(){
-    alertify.alert('Information','The color is the background is composed from the hexadecimal of the hour following this format "#hhmmss", this project is open source, which mean you can check it on <a  target="_blank" href="https://github.com/Okuuu/TimeColor">GitHub</a>')
+    alertify.alert('Information',
+        'The color is the background is composed from the hexadecimal of the time ' +
+        'following this format "#hhmmss", this project is open source, ' +
+        'which mean you can check it on ' +
+        '<a  target="_blank" href="https://github.com/Okuuu/TimeColor">GitHub</a>')
 }
 //TODO: Add a copy to clipboard function
 
+function copy () {
+    var div = document.createElement("div");
+    var obj = getColor();
+
+    div.innerHTML= obj.color;
+    storage.appendChild(div);
+    div.setAttribute("contentEditable", true);
+    div.setAttribute("onfocus", "document.execCommand('selectAll',false,null)");
+    div.focus();
+
+    var successful = document.execCommand('copy');
+    if(successful){
+        alertify.success('Copied !');
+        div.remove();
+    }
+    else{
+        alertify.error('An error occured, please retry')
+    }
+}
+
+function getColor() {
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+    var color = '#' +  hours + minutes + seconds;
+    //Color contain a string with the hexadecimal code of the color
+
+    //Returning an object which contains all the information we need
+    return {
+        color: color,
+        hours : ''+hours,
+        minutes : ''+minutes,
+        seconds : ''+seconds
+    }
+}
 
